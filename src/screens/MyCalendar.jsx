44e3f227dc0371ee7dc63eebfbacb9983f6cd4e5
Masum Billah <fabrika786@gmail.com>
2024-03-03 21:17:22 +0600

    my and subojit code added

--- src/screens/MyCalendar.jsx
+++ src/screens/MyCalendar.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
-import {  ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-// import Calendar  from 'react-native-calendars/src/calendar';
-import { Calendar, LocaleConfig } from 'react-native-calendars';
+import React, { useState } from "react";
 import {
-    responsiveScreenHeight,
-    responsiveScreenWidth,
-    responsiveScreenFontSize,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+// import Calendar  from 'react-native-calendars/src/calendar';
+import { Calendar, LocaleConfig } from "react-native-calendars";
+import {
+  responsiveScreenHeight,
+  responsiveScreenWidth,
+  responsiveScreenFontSize,
 } from "react-native-responsive-dimensions";
 
-import { useFonts } from 'expo-font';
-import LineSvgComponent from '../svg/LineSvgComponent';
-import DotComponent from '../components/DotComponent';
+import { useFonts } from "expo-font";
+import LineSvgComponent from "../svg/LineSvgComponent";
+import DotComponent from "../components/DotComponent";
 // calendar Heading......................................
 
 import { useNavigation } from "@react-navigation/native";
 import PlusCircleSvgComponent from "../svg/PlusCircleSvgComponent";
-
+import RightArrowLong from "../svg/RightArrowLong";
 
 export const CalendarHeading = () => {
   const navigation = useNavigation();
@@ -28,19 +35,17 @@ export const CalendarHeading = () => {
   return (
     <View style={calenderHeadingStyles.header}>
       <View style={calenderHeadingStyles.headerContent}>
-      <Text style={calenderHeadingStyles.title}>My Calendar</Text>
+        <Text style={calenderHeadingStyles.title}>My Calendar</Text>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <View style={calenderHeadingStyles.btn}>
-            <PlusCircleSvgComponent/>
-          <Text style={calenderHeadingStyles.btnText}>New Event</Text>
+            <PlusCircleSvgComponent />
+            <Text style={calenderHeadingStyles.btnText}>New Event</Text>
           </View>
         </TouchableOpacity>
-     
       </View>
     </View>
   );
 };
-
 
 const calenderHeadingStyles = StyleSheet.create({
   header: {
@@ -53,7 +58,7 @@ const calenderHeadingStyles = StyleSheet.create({
 
     marginLeft: responsiveScreenWidth(5),
     flexDirection: "row",
-    justifyContent:'space-between',
+    justifyContent: "space-between",
     gap: 10,
     alignItems: "center",
   },
@@ -64,120 +69,32 @@ const calenderHeadingStyles = StyleSheet.create({
     fontFamily: "Inter-Medium",
     fontWeight: "500",
   },
-  btn:{
-    width:responsiveScreenWidth(30),
-    height:responsiveScreenHeight(5),
+  btn: {
+    width: responsiveScreenWidth(30),
+    height: responsiveScreenHeight(5),
     backgroundColor: "#27AC1F",
-    flexDirection:'row',
-    alignItems:'center',
-  justifyContent:'space-between',
-  paddingHorizontal:responsiveScreenWidth(4),
-  gap:8,
-  borderRadius:responsiveScreenWidth(2)
-
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: responsiveScreenWidth(4),
+    gap: 8,
+    borderRadius: responsiveScreenWidth(2),
   },
-  btnText:{
-    color:'white'
-  }
+  btnText: {
+    color: "white",
+  },
 });
 
+//...........................................
 
-// Main Components.......................................
-
-
-const MyCalendar = () => {
-
-    //.......................For using google font................//
-    const [fontsLoaded] = useFonts({
-        "WorkSans-Regular": require("../../assets/Fonts/WorkSans-Regular.ttf"),
-        "WorkSans-Black": require("../../assets/Fonts/WorkSans-Black.ttf"),
-        "WorkSans-Medium": require("../../assets/Fonts/WorkSans-Medium.ttf"),
-    });
-    if (!fontsLoaded) {
-        return <Text>Loading Font...</Text>;
-    }
-    return (
-        <View style={styles.container}>
-            <CalendarHeading/>
-            <ScrollView>
-            <View>
-                <Calendar
-                 onDayPress={date=>console.log(date)}
-             
-                 markedDates={{
-                    '2024-03-20': {marked: true,dotColor:'red',selected:true, selectedColor:'#e9f7e9', selectedTextColor:'#27ac1f'},
-                    '2024-03-10': {marked: true,dotColor:'red',backgroundColor:'red'},
-                    '2024-03-07': {marked: true,dotColor:'#7666ff',backgroundColor:'red'},
-                  }}
-                 hideExtraDays ={true}
-                 style={{
-             
-                    height: 350
-                  }}
-                  theme={{
-                    backgroundColor: '#ffffff',
-                    calendarBackground: '#ffffff',
-                    textSectionTitleColor: '#b6c1cd',
-                    selectedDayBackgroundColor: '#00adf5',
-                    selectedDayTextColor: '#ffffff',
-                    todayTextColor: 'green',
-                    dayTextColor: '#2d4150',
-                    textDisabledColor: '#d9e'}}
-                 />
-            </View>
-<DotComponent/>
-
-<View style={styles.line}>
-    <LineSvgComponent/>
-</View>
-
-            </ScrollView>
-        </View>
-    );
-};
-
-export default MyCalendar;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor:'white',
-        paddingHorizontal:responsiveScreenWidth(4)
-
-    },
-    dotContainer:{
-        flexDirection:'row', alignItems:'center', gap:responsiveScreenWidth(14),paddingHorizontal:responsiveScreenWidth(4),
-        paddingTop:responsiveScreenHeight(1)
-    },
-    dotCon:{
-        flexDirection:'row', alignItems:'center', gap:responsiveScreenWidth(11),paddingHorizontal:responsiveScreenWidth(4),
-        paddingTop:responsiveScreenHeight(1)
-    },
-    dot:{flexDirection:'row', alignItems:'center', gap:8},
-    line:{
-        paddingTop:responsiveScreenHeight(2),
-        alignSelf:'center'
-    }
-
-});
-
-
-
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import {
-  responsiveScreenFontSize,
-  responsiveScreenHeight,
-  responsiveScreenWidth,
-} from "react-native-responsive-dimensions";
-import RightArrowLong from "../../../assets/svgs/RightArrowLong";
-import Fonts from "../../../assets/Fonts/Fonts";
+// import RightArrowLong from "../../../assets/svgs/RightArrowLong";
+// import Fonts from "../../../assets/Fonts/Fonts";
 
 // --------------------------
 // ----------- Calendar Item Components -----------
 // --------------------------
 
- const CalendarItems = (item) => {
+export const CalendarItems = (item) => {
   return (
     <View style={styles.eventTopicContainer}>
       <View style={styles.eventDateContainer}>
@@ -233,7 +150,7 @@ import Fonts from "../../../assets/Fonts/Fonts";
 // ----------- Main Components -----------
 // --------------------------
 
-const Calendar = () => {
+export const CalendarData = () => {
   const data = [
     {
       title: "Show N Tell (Finished)",
@@ -273,43 +190,43 @@ const Calendar = () => {
   ];
   return (
     <ScrollView>
-      <View style={styles.calenderTopicContainer}>
-        <View style={styles.weekContainer}>
-          <Text style={styles.weekText}>Week 1</Text>
-          <Text style={styles.weekText}>Jan 1 - Jan 7</Text>
+      <View style={CalendarStyles.calenderTopicContainer}>
+        <View style={CalendarStyles.weekContainer}>
+          <Text style={CalendarStyles.weekText}>Week 1</Text>
+          <Text style={CalendarStyles.weekText}>Jan 1 - Jan 7</Text>
         </View>
         {data.map(
           (item, index) =>
             item.date >= 1 && item.date <= 7 && <CalendarItems item={item} />
         )}
 
-        <View style={styles.weekContainer}>
-          <Text style={styles.weekText}>Week 2</Text>
-          <Text style={styles.weekText}>Jan 8 - Jan 14</Text>
+        <View style={CalendarStyles.weekContainer}>
+          <Text style={CalendarStyles.weekText}>Week 2</Text>
+          <Text style={CalendarStyles.weekText}>Jan 8 - Jan 14</Text>
         </View>
         {data.map(
           (item, index) =>
             item.date >= 8 && item.date <= 14 && <CalendarItems item={item} />
         )}
-        <View style={styles.weekContainer}>
-          <Text style={styles.weekText}>Week 3</Text>
-          <Text style={styles.weekText}>Jan 15 - Jan 21</Text>
+        <View style={CalendarStyles.weekContainer}>
+          <Text style={CalendarStyles.weekText}>Week 3</Text>
+          <Text style={CalendarStyles.weekText}>Jan 15 - Jan 21</Text>
         </View>
         {data.map(
           (item, index) =>
             item.date >= 15 && item.date <= 21 && <CalendarItems item={item} />
         )}
-        <View style={styles.weekContainer}>
-          <Text style={styles.weekText}>Week 4</Text>
-          <Text style={styles.weekText}>Jan 22 - Jan 28</Text>
+        <View style={CalendarStyles.weekContainer}>
+          <Text style={CalendarStyles.weekText}>Week 4</Text>
+          <Text style={CalendarStyles.weekText}>Jan 22 - Jan 28</Text>
         </View>
         {data.map(
           (item, index) =>
             item.date >= 22 && item.date <= 28 && <CalendarItems item={item} />
         )}
-        <View style={styles.weekContainer}>
-          <Text style={styles.weekText}>Week 5</Text>
-          <Text style={styles.weekText}>Jan 29 - Feb 4</Text>
+        <View style={CalendarStyles.weekContainer}>
+          <Text style={CalendarStyles.weekText}>Week 5</Text>
+          <Text style={CalendarStyles.weekText}>Jan 29 - Feb 4</Text>
         </View>
         {data.map(
           (item, index) =>
@@ -320,9 +237,7 @@ const Calendar = () => {
   );
 };
 
-export default Calendar;
-
-const styles = StyleSheet.create({
+const CalendarStyles = StyleSheet.create({
   eventDay: {
     color: "#546A7E",
   },
@@ -368,5 +283,99 @@ const styles = StyleSheet.create({
     gap: responsiveScreenHeight(1.5),
     // width: "100%",
     backgroundColor: "white",
-  },
+  },
+});
+
+// Main Components.......................................
+
+const MyCalendar = () => {
+  //.......................For using google font................//
+  const [fontsLoaded] = useFonts({
+    "WorkSans-Regular": require("../../assets/Fonts/WorkSans-Regular.ttf"),
+    "WorkSans-Black": require("../../assets/Fonts/WorkSans-Black.ttf"),
+    "WorkSans-Medium": require("../../assets/Fonts/WorkSans-Medium.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <Text>Loading Font...</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <CalendarHeading />
+      <ScrollView>
+        <View>
+          <Calendar
+            onDayPress={(date) => console.log(date)}
+            markedDates={{
+              "2024-03-20": {
+                marked: true,
+                dotColor: "red",
+                selected: true,
+                selectedColor: "#e9f7e9",
+                selectedTextColor: "#27ac1f",
+              },
+              "2024-03-10": {
+                marked: true,
+                dotColor: "red",
+                backgroundColor: "red",
+              },
+              "2024-03-07": {
+                marked: true,
+                dotColor: "#7666ff",
+                backgroundColor: "red",
+              },
+            }}
+            hideExtraDays={true}
+            style={{
+              height: 350,
+            }}
+            theme={{
+              backgroundColor: "#ffffff",
+              calendarBackground: "#ffffff",
+              textSectionTitleColor: "#b6c1cd",
+              selectedDayBackgroundColor: "#00adf5",
+              selectedDayTextColor: "#ffffff",
+              todayTextColor: "green",
+              dayTextColor: "#2d4150",
+              textDisabledColor: "#d9e",
+            }}
+          />
+        </View>
+        <DotComponent />
+
+        <View style={styles.line}>
+          <LineSvgComponent />
+        </View>
+        <CalendarData />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MyCalendar;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingHorizontal: responsiveScreenWidth(4),
+  },
+  dotContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: responsiveScreenWidth(14),
+    paddingHorizontal: responsiveScreenWidth(4),
+    paddingTop: responsiveScreenHeight(1),
+  },
+  dotCon: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: responsiveScreenWidth(11),
+    paddingHorizontal: responsiveScreenWidth(4),
+    paddingTop: responsiveScreenHeight(1),
+  },
+  dot: { flexDirection: "row", alignItems: "center", gap: 8 },
+  line: {
+    paddingTop: responsiveScreenHeight(2),
+    alignSelf: "center",
+  },
 });
